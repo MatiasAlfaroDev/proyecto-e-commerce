3cbbf389e868d15a49530b2a12dbf45cecbc7fa7
Matías Alfaro <matiassnts@gmail.com>
2022-08-20 01:08:51 -0300

    Login form

---
+++ js/login.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.querySelector('#logBtn');
+
+    boton.addEventListener('click', (event) => {
+        console.log('click')
+        event.preventDefault();
+
+        const email = document.querySelector('#email')
+        const password= document.querySelector('#password')
+
+        console.log(password)
+       
+        if (email.value == '' || password.value == '') {
+            document.getElementById('password').classList.add('is-invalid');
+            document.getElementById('email').classList.add('is-invalid');
+        } else {
+            location.replace("home.html")
+            
+        }     
+    })
+})
